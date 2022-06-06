7eb0939bfd9fd92c49c8ad2ac270a59c81d2eaa5
Samuel Miller <smillercb@gmail.com>
2022-06-06 13:39:10 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿List<object> myList=new List<object>();
+myList.Add(7);
+myList.Add(28);
+myList.Add(-1);
+myList.Add(true);
+myList.Add("chair");
+int sum=0;
+for (int i=0;i<myList.Count;i++){
+    Console.WriteLine(myList[i]);
+    if(myList[i] is int){
+        sum+=(int)myList[i];
+    }
+}
+Console.WriteLine(sum);
